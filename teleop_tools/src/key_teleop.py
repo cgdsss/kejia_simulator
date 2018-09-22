9856e51c74ea199f0cd9978569f285cf111eee6d
cgd_pc <cgdsss@mail.ustc.edu.cn>
2018-09-22 17:34:10 +0800

    new model and world, fix kejia control

--- teleop_tools/src/key_teleop.py
+++ teleop_tools/src/key_teleop.py
@@ -176,7 +176,7 @@ class SimpleKeyTeleop():
 
         self._forward_rate = rospy.get_param('~forward_rate', 0.8)
         self._backward_rate = rospy.get_param('~backward_rate', 0.5)
-        self._rotation_rate = rospy.get_param('~rotation_rate', 1.0)
+        self._rotation_rate = rospy.get_param('~rotation_rate', 0.5)
         self._last_pressed = {}
         self._angular = 0
         self._linear = 0
